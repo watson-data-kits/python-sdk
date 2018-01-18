f3a6b450e7910df1f1eb3f215b88999e40c4b933
Joe Meissler <joe.meissler@gmail.com>
2018-01-17 16:44:27 -0800

    python requires, v1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='watson_data_kits',
-      version='1.0',
+      version='1.1',
       description='Python wrapper for Watson Data Kits',
       url='https://github.com/watson-data-kits/python-sdk',
       author='Joe Meissler',
@@ -11,4 +11,5 @@ setup(name='watson_data_kits',
       install_requires=[
           'requests>=2.18.4,<2.19.0'
       ],
+      python_requires='~=3.3',
       zip_safe=False)
