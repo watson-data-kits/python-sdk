94637f7a4f7017235da32914a1b2de188260b228
Joe Meissler <joe.meissler@gmail.com>
2018-01-17 16:04:26 -0800

    update setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='watson_data_kits',
-      version='0.1',
+      version='1.0',
       description='Python wrapper for Watson Data Kits',
       url='https://github.com/watson-data-kits/python-sdk',
       author='Joe Meissler',
       author_email='joe@us.ibm.com',
-      license='MIT',
+      license='Apache License, Version 2.0',
       packages=['watson_data_kits'],
       install_requires=[
           'requests>=2.18.4,<2.19.0'
